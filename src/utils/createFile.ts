6d91e7fc728ed49b23690dcbe0170483271c7383
francis <dominiquefrancis42@gmail.com>
2025-12-28 21:00:21 +0800

    feat: git setup

--- src/utils/createFile.ts
+++ src/utils/createFile.ts
@@ -1,11 +1,15 @@
 import { join } from "node:path";
 import fs, { ensureFile } from "fs-extra";
+import { setupGit } from "./setupGit.js";
+import chalk from "chalk";
 
 export const createFiles = async (projectPath: string, files: string[]) => {
   try {
     await Promise.all(
       files.map((file) => fs.ensureFile(join(projectPath, file)))
     );
+    console.log(chalk.blue("Setting up git..."));
+    await setupGit(projectPath);
   } catch (err) {
     console.log(err);
   }
