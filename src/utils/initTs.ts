291c02960c63397ab74c3e340e9cd4f688378179
francis <dominiquefrancis42@gmail.com>
2026-02-13 22:15:19 +0800

    fix: correct execa command in initializeTs

--- src/utils/initTs.ts
+++ src/utils/initTs.ts
@@ -2,7 +2,7 @@ import { execa } from "execa";
 
 export const initializeTs = async (projectPath: string) => {
   try {
-    await execa("exec", ["tsc", "--init"], { cwd: projectPath });
+    await execa("pnpm", ["exec", "tsc", "--init"], { cwd: projectPath });
   } catch (error) {
     console.log("Failed to initialize typescript", error);
   }
